c19be4f35c899855888ffe22473e55fed78437bf
lieziyn_tyan <eliz.code@mail.ru>
2020-11-16 12:37:53 +0700

    Добавление 1 задачи

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const buttonAuth = document.querySelector('.button-add__button');
+const NotTasks = document.querySelector('.todo-list__not');
+const modalCloseAuth = document.querySelector('.buttons__close');
+const modalAddAuth = document.querySelector('.buttons__add');
 const modalAuth = document.querySelector('.modal-auth');
-const modal = document.querySelector(".todo-list");
+const modal = document.querySelector('.todo-list');
+const TaskInput = document.querySelector('.modal-auth__input');
+const TaskEdit = document.querySelector('.header__button');
+const ToDoListItem = document.querySelector('.todo-list-item');
 
 
 
@@ -13,9 +19,34 @@ const toggleModalAuth = function () {
 		enableScroll();
 	}
 }
+const modalAddTask = function () {
+	modal.textContent = '';
+	let Task = new Object();
+	Task.Tasktext = TaskInput.value;
+	
+	const itemTask = `
+	<label class="todo-list-item todo-list__label">
+		<input  class="todo-list-item__checkbox " type="checkbox">
+		<span>${Task.Tasktext}</span> 
+	</label> 
+		`;
+	modal.insertAdjacentHTML('afterbegin', itemTask)
+	NotTasks.remove();
+	modalAuth.classList.toggle('is-open');
+}
 
-buttonAuth.addEventListener('click', toggleModalAuth);
+
+
+const modalEditTask = function () {
+	TaskEdit.style.text = "Отменить";
+}
+
 
 const toggleModal = function () {
 	modal.classList.toggle("is-open");
 }
+
+buttonAuth.addEventListener('click', toggleModalAuth);
+modalCloseAuth.addEventListener('click', toggleModalAuth);
+modalAddAuth.addEventListener('click', modalAddTask);
+TaskEdit.addEventListener('click', modalEditTask);
